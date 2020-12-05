0fa7f28fa7652809330a11393528f5e8fe10c3a3
tieuanh <nguyentieuanh307@gmail.com>
2020-12-05 10:22:16 +0700

    check

--- src/com/company/TestCase5.java
+++ src/com/company/TestCase5.java
@@ -4,13 +4,9 @@ public class TestCase5 {
     public static void Unittest() throws Exception{
         System.out.println("Test case 5: Phonenumber invalid");
         String phonenumber = "9745538291";
-        char comp = phonenumber.charAt(0);
-        if(!("0".equals(comp)) || !(phonenumber.length()==9)){
-            System.out.println("Phonenumber is invalid: not satisfied length or not begin with 0");
-        }
         ResponseGetVCAPI res = getvcAPI.callAPI(phonenumber);
-        if ((!"1004".equals(res.code))) throw new AssertionError();
-        if ((!"Parameter valus is invalid".equals(res.message))) throw new AssertionError();
+        if ((!"1003".equals(res.code))) throw new AssertionError();
+        if ((!"Parameter type is invalid".equals(res.message))) throw new AssertionError();
 
         System.out.println("Finished!");
     }
